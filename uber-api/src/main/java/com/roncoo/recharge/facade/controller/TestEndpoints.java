a2098f684e49fe8272a95bc687f0b42eb455f4d9
谢荣雳 <xrl6238@arcsoft.com.cn>
2019-04-28 14:37:10 +0800

    redis 测试代码

--- uber-api/src/main/java/com/roncoo/recharge/facade/controller/TestEndpoints.java
+++ uber-api/src/main/java/com/roncoo/recharge/facade/controller/TestEndpoints.java
@@ -1,5 +1,7 @@
 package com.roncoo.recharge.facade.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Api(tags = "1.1", description = "测试", value = "测试")
 public class TestEndpoints {
 
+    @ApiOperation(value = "获取产品",notes = "获取产品")
     @GetMapping("/product/{id}")
     public String getProduct(@PathVariable String id) {
         //for debug
@@ -16,10 +20,13 @@ public class TestEndpoints {
         return "product id : " + id;
     }
 
+    @ApiOperation(value = "获取产品",notes = "获取产品")
     @GetMapping("/order/{id}")
     public String getOrder(@PathVariable String id) {
         //for debug
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return "order id : " + id;
     }
+
+
 }
