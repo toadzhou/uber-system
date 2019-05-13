6a817ed149325516ecebc4ad42f618606bc3fdbd
xiaoxuda <xiaoxuda@duiba.com.cn>
2019-05-13 19:30:56 +0800

    add

---
+++ uber-api/src/test/java/com/roncoo/recharge/test/Model.java
@@ -0,0 +1,18 @@
+package com.roncoo.recharge.test;
+
+import lombok.Data;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * Description:
+ *
+ * @author xierongli
+ * @create 2019/5/13 15:15
+ */
+@Data
+public class Model {
+
+    private String ua;
+
+
+}
