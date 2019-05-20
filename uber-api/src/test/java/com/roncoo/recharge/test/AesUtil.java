3c2357146725f6a244b1e5a30a821c289ef26ba8
谢荣雳 <xrl6238@arcsoft.com.cn>
2019-05-20 15:02:34 +0800

    redis 测试代码

--- uber-api/src/test/java/com/roncoo/recharge/test/AesUtil.java
+++ uber-api/src/test/java/com/roncoo/recharge/test/AesUtil.java
@@ -1,17 +1,13 @@
 package com.roncoo.recharge.test;
 
-import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
 import java.io.IOException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
 
 /**
  * 〈一句话功能简述〉<br>
@@ -22,26 +18,15 @@ import java.security.SecureRandom;
  */
 public class AesUtil {
 
-    /**
-     * 加解密价格放在http请求中，避免出现http中关键字报错，蜻蜓方替换掉关键字
-     *
-     * @param data
-     * @return
-     */
+
     public static String safeUrlBase64Encode(byte[] data) {
-        String encodeBase64 = new BASE64Encoder().encode(data);
+        String encodeBase64 = Base64.getEncoder().encodeToString(data);
         String safeBase64Str = encodeBase64.replace('+', '-');
         safeBase64Str = safeBase64Str.replace('/', '_');
         safeBase64Str = safeBase64Str.replaceAll("=", "");
         return safeBase64Str;
     }
 
-    /**
-     * 加解密价格放在http请求中，避免出现http中关键字报错，蜻蜓方替换掉关键字
-     *
-     * @param safeBase64Str
-     * @return
-     */
     public static byte[] safeUrlBase64Decode(final String safeBase64Str) throws IOException {
         String base64Str = safeBase64Str.replace('-', '+');
         base64Str = base64Str.replace('_', '/');
@@ -49,7 +34,7 @@ public class AesUtil {
         if (mod4 > 0) {
             base64Str = base64Str + "====".substring(mod4);
         }
-        byte[] result = new BASE64Decoder().decodeBuffer(base64Str);
+        byte[] result = Base64.getDecoder().decode(base64Str);
         return result;
     }
 
@@ -65,32 +50,12 @@ public class AesUtil {
     public static String decrypt(String encode, String key) throws Exception {
         byte[] encrypted = safeUrlBase64Decode(encode);
         byte[] enCodeFormat = key.getBytes();
-        SecretKeySpec skeySpec = initKeyForAES(key);
+        SecretKeySpec skeySpec = new SecretKeySpec(enCodeFormat, "AES");
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, skeySpec);
         byte[] original = cipher.doFinal(encrypted);
         String originalString = new String(original, "utf-8");
         return originalString;
-    }
-
-    private static SecretKeySpec initKeyForAES(String key) throws NoSuchAlgorithmException {
-        if (null == key || key.length() == 0) {
-            throw new NullPointerException("key not is null");
-        }
-        SecretKeySpec key2 = null;
-        SecureRandom random = SecureRandom.getInstance("SHA1PRNG");
-        random.setSeed(key.getBytes());
-        try {
-            KeyGenerator kgen = KeyGenerator.getInstance("AES");
-            kgen.init(128, random);
-            SecretKey secretKey = kgen.generateKey();
-            byte[] enCodeFormat = secretKey.getEncoded();
-            key2 = new SecretKeySpec(enCodeFormat, "AES");
-        } catch (NoSuchAlgorithmException ex) {
-            throw new NoSuchAlgorithmException();
-        }
-        return key2;
-
     }
 
     public static void main(String[] args) throws Exception {
