ff61fe407ff42ae10a34baa40d0cc5a68ec7545b
谢荣雳 <xrl6238@arcsoft.com.cn>
2019-07-23 21:24:48 +0800

    excel

--- uber-api/src/test/java/com/roncoo/recharge/test/demo/GoodsServiceTest.java
+++ uber-api/src/test/java/com/roncoo/recharge/test/demo/GoodsServiceTest.java
@@ -1,6 +1,7 @@
 package com.roncoo.recharge.test.demo;
 
 import com.alibaba.fastjson.JSON;
+import com.roncoo.recharge.FacadeApplication;
 import com.roncoo.recharge.facade.service.GoodsService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,12 +12,19 @@ import org.springframework.test.context.junit4.SpringRunner;
 /**
  * 〈一句话功能简述〉<br>
  * Description:
+ * WebEnvironment.MOCK—提供一个Mock的Servlet环境，内置的Servlet容器并没有真实的启动，主要搭配使用@AutoConfigureMockMvc
+ *
+ * WebEnvironment.RANDOM_PORT — 提供一个真实的Servlet环境，也就是说会启动内置容器，然后使用的是随机端口
+ *
+ * WebEnvironment.DEFINED_PORT — 这个配置也是提供一个真实的Servlet环境，使用的默认的端口，如果没有配置就是8080
+ *
+ * WebEnvironment.NONE — 这是个神奇的配置，跟Mock一样也不提供真实的Servlet环境。
  *
  * @author xierongli
  * @create 2019/5/27 17:41
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = FacadeApplication.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class GoodsServiceTest {
 
     @Autowired
--- uber-web/src/test/java/com/roncoo/recharge/Test.java
+++ uber-web/src/test/java/com/roncoo/recharge/Test.java
@@ -1,11 +1,18 @@
 package com.roncoo.recharge;
 
 
+import cn.hutool.core.io.FileUtil;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
 import com.alibaba.fastjson.JSON;
+import org.apache.commons.io.FileUtils;
+import org.springframework.util.ResourceUtils;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 /**
  * @author xierongli
@@ -15,9 +22,22 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args) {
+        try {
+            File jsonFile = ResourceUtils.getFile("classpath:slot_baidu.json");
+            String json = FileUtils.readFileToString(jsonFile,"utf-8");
+            System.out.println(json);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
+    public void  readExcel(){
         ExcelReader reader1 = ExcelUtil.getReader("a.xlsx");
-
-
         reader1.addHeaderAlias("类型","adName")
                 .addHeaderAlias("广告位id", "slotId")
                 .addHeaderAlias("广告位中文名称","slotName")
