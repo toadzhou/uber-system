da066f06264607cd0d930b3824b732f70335599f
xiaoxuda <xiaoxuda@duiba.com.cn>
2019-07-23 10:00:16 +0800

    fix

---
+++ uber-web/src/test/java/com/roncoo/recharge/Ad.java
@@ -0,0 +1,22 @@
+package com.roncoo.recharge;
+
+import lombok.Data;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * Description:
+ *
+ * @author xierongli
+ * @create 2019/7/23 9:46
+ */
+@Data
+public class Ad {
+
+    private String adName;
+
+    private String slotId;
+
+    private String slotName;
+
+    private String floorPrice;
+}
--- uber-web/src/test/java/com/roncoo/recharge/Test.java
+++ uber-web/src/test/java/com/roncoo/recharge/Test.java
@@ -1,9 +1,9 @@
 package com.roncoo.recharge;
 
 
-import cn.hutool.core.lang.Console;
+import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
-import cn.hutool.poi.excel.sax.handler.RowHandler;
+import com.alibaba.fastjson.JSON;
 
 import java.util.List;
 
@@ -15,17 +15,17 @@ import java.util.List;
 public class Test {
 
     public static void main(String[] args) {
+        ExcelReader reader1 = ExcelUtil.getReader("a.xlsx");
 
-        ExcelUtil.read07BySax("/Users/mark1xie/workspace/uber-system/uber-web/src/test/resources/a.xlsx", 0, createRowHandler());
-//        ExcelUtil.readBySax("/Users/mark1xie/workspace/uber-system/uber-web/src/test/resources/a.xlsx", 0, createRowHandler());
+
+        reader1.addHeaderAlias("类型","adName").addHeaderAlias("广告位Id", "slotId")
+                .addHeaderAlias("广告位中文名称","slotName")
+                .addHeaderAlias("底价", "floorPrice");
+        List<Ad> adList = reader1.readAll(Ad.class);
+        reader1.close();
+        System.out.println(JSON.toJSONString(adList));
+
     }
 
-    private static  RowHandler createRowHandler() {
-        return new RowHandler() {
-            @Override
-            public void handle(int sheetIndex, int rowIndex, List<Object> rowlist) {
-                Console.log("[{}] [{}] {}", sheetIndex, rowIndex, rowlist);
-            }
-        };
-    }
+
 }
